2c5ad87b4d67565151ed74086e016be89fdf56df
Ricardo Santos <ri.santos@hotmail.com>
2022-05-19 21:52:32 -0300

    trabalhando com states

--- AppReactCourse/src/pages/StatesReact/index.js
+++ AppReactCourse/src/pages/StatesReact/index.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React , {Component} from 'react';
 import {View , Text , StyleSheet, Button} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
-export default function StateReact() {
-    const navigation = useNavigation();
+export default class  StateReact extends Component{
+    
+    constructor(props){
+        super(props);
 
-    return(
-        <View>
-            <Text>Trabalhando com propriedades no States no react  </Text>
-            <Button title="Menu" onPress={() => navigation.navigate('Menu')}/>
-        </View>
 
-    );
+        this.state = {
+            nome:''
+        }
+
+        this.mudarNome = this.mudarNome.bind(this);
+    }
+   
+
+    mudarNome(nome){
+        this.setState({
+            nome:nome
+        })
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <Text>Alterando texto {this.state.nome} </Text>
+                <Text style={styles.textoNormal}>TEXTO 1 </Text>
+                <Text style={styles.textoAlinhado}>TEXTO 2 </Text>
+                <Text style={[styles.textoNormal , styles.textoAlinhado] }> COM TODAS AS FORMATACOES  </Text>
+
+                <Button title="Alterar nome" onPress={() => this.mudarNome('fdsjlkfjas')}/>
+                <Button title="Menu" onPress={() => this.props.navigation.navigate('Menu')}/>
+            </View>    
+        );
+    }    
 }
 
 const styles = StyleSheet.create({
     container: {
         flex:1,
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
+
+    },
+    textoNormal:{
+        fontSize:30,
+        color:'red'
+    },
+    textoAlinhado:{
+        textAlign:'center'
     }
 })
